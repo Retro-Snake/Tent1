3251fc59a0d1f47abfaa664ca3d41264a5b43239
Retro-Snake <kotdim2000@yandex.ru>
2023-10-21 23:28:56 +0300

    Revert "Merge branch 'main' of https://github.com/Retro-Snake/Tent1"
    This reverts commit ffd9b4375e49afb2fe644ffea84d014027d34f0e, reversing
    changes made to 2238c920497e2bf65142c86a771c40a5f1c223cd.

--- Assets/Scripts/UpdateScore.cs
+++ Assets/Scripts/UpdateScore.cs
@@ -4,37 +4,24 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SocialPlatforms.Impl;
-using System;
 
 public class UpdateScore : MonoBehaviour
 {
     public TextMeshProUGUI TextScore;    
     public int currentScore = 0;
-    public event Action<int> OnScoreChanged; // Событие для отслеживания
-
-
+        
     public void UpdateNumberText(string lvlName,int scorReset)
     {
         currentScore++; 
         currentScore = scorReset * currentScore; 
-        TextScore.text = currentScore.ToString("000");        
-        
+        TextScore.text = currentScore.ToString("000");
         PlayerPrefs.SetInt(lvlName, currentScore);
         PlayerPrefs.Save();
-
     }
     public void LoadScore(string lvlName)
     {
         int actualScore = PlayerPrefs.GetInt(lvlName); 
         currentScore = actualScore;
-        TextScore.text = actualScore.ToString("000");
-        ScoreInvokeUpdate();
+        TextScore.text = actualScore.ToString("000"); 
     }
-
-    public void ScoreInvokeUpdate()
-    {
-        OnScoreChanged?.Invoke(currentScore);// Вызываем событие, чтобы оповестить другие части кода об изменении переменной
-
-    }
-
 }
