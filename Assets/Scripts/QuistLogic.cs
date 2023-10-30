6a9127dc97521aa05e9176f68113030fd46bbbd8
Retro-Snake <kotdim2000@yandex.ru>
2023-10-30 23:14:08 +0300

    Retro

--- Assets/Scripts/QuistLogic.cs
+++ Assets/Scripts/QuistLogic.cs
@@ -8,14 +8,16 @@ public class QuistLogic : MonoBehaviour
     public int curscore;
     public int allscore;
     public float zNew;
+    public float yBack;
     private bool chekQuestIndex = true; // True = первое уведомление можно пускать. False = первое уведомление не надо пускать уже было
     private Vector3 zOld;
     private Vector3 currentPosZ;
+    private Vector3 yBC;
     public GameObject questGameObject;// Игровой объект который содержит в себе предметы , что должны появится по завершению квеста
     public GameObject fonZadnik; // Заглушка что будет перекрывать визально предмет
     [Header("Список Объектов что необходимы для прохождения квеста")]
     public List<GameObject> QuestListObject;//Список Объектов что необходимы для прохождения квеста
-    private List<GameObject> CurrentQuestListObject = new List<GameObject>();//Список выбранных объектов игроком уже,нужен для проверки
+    public List<GameObject> CurrentQuestListObject = new List<GameObject>();//Список выбранных объектов игроком уже,нужен для проверки
     [Header("Сообщение О начале квеста")]
     public string questNowNotif;
     [Header("Сообщение положительное Первое")]
@@ -27,8 +29,9 @@ public class QuistLogic : MonoBehaviour
     {
         currentPosZ = transform.position;
         questGameObject.transform.position = currentPosZ;
+        yBC.y = yBack;
         zOld = transform.position;
-
+        questGameObject.transform.position = yBC;
         scoreUpdate = FindObjectOfType<UpdateScore>();
         scoreUpdate.OnScoreChanged += MyCustomMetod;// Подписка на событие изменения количества щупалец и вызов метода
     }
@@ -49,16 +52,29 @@ public class QuistLogic : MonoBehaviour
                 chekQuestIndex = false;
                 Debug.Log("Мы тут 1");
             }
-            fonZadnik.SetActive(false);
+            ActivateChildrenRecursively(fonZadnik.transform, false);
         }
         else
         {
             transform.position = zOld;
             questGameObject.transform.position = zOld;
             chekQuestIndex = true;
-            fonZadnik.SetActive(true);
+            ActivateChildrenRecursively(fonZadnik.transform, true);                        
+            Debug.Log("Мы тут 55");
         }      
         
+    }
+
+    void ActivateChildrenRecursively(Transform parent,bool checkBool)
+    {
+        // Активируем текущий дочерний объект
+        parent.gameObject.SetActive(checkBool);
+
+        // Проходимся по всем дочерним объектам текущего объекта и активируем их рекурсивно
+        foreach (Transform child in parent)
+        {
+            ActivateChildrenRecursively(child, checkBool);
+        }
     }
 
     public void CorrectObjectCheck( GameObject gameObject)// Проверка на соответвие коллекций, добавлем и удаляем если объект уже был в нашейколлекции, если число объектов одного листа соответсвует второму проводим сравнение объектов в листах
@@ -70,24 +86,31 @@ public class QuistLogic : MonoBehaviour
         {
          CurrentQuestListObject.Remove(gameObject);
         }
-
-        if(CurrentQuestListObject.Count == QuestListObject.Count) 
+        if(CurrentQuestListObject.Count < QuestListObject.Count)
         {
+            Debug.Log("попали в проверку1");           
+            questGameObject.transform.position = yBC;
+        }else if(CurrentQuestListObject.Count == QuestListObject.Count) 
+         {
+            Debug.Log("попали в проверку");
             bool allObjectsSelected = true;
             foreach (GameObject QuestObject in QuestListObject)
             {
-                if(!CurrentQuestListObject.Contains(QuestObject)) 
+                Debug.Log("сейчас щупальца трогать нельзя");
+                if (!CurrentQuestListObject.Contains(QuestObject)) 
                 {
                     if (FindObjectOfType<NotificationInvoke>() != null)
                     {
                         FindObjectOfType<NotificationInvoke>().NotifInvoke(GetRandomPhrase(QuestBadInvokeList));
                     }
+                    questGameObject.transform.position = zOld;
                     allObjectsSelected = false;
                     break;
                 }
             }
             if (allObjectsSelected)//Если Листы одинаковые то появляется завершение квеста
             {
+                Debug.Log("Тут квест пройден и появляются щупальца");
                 questGameObject.transform.position = currentPosZ;
                 if (FindObjectOfType<NotificationInvoke>() != null)
                 {
@@ -95,7 +118,7 @@ public class QuistLogic : MonoBehaviour
                 }
 
             }
-        }        
+         }        
     }
      
     
