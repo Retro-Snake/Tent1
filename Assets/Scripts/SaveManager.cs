1aa772425b04d78eefc5bc9a15552eeee909000b
Retro-Snake <kotdim2000@yandex.ru>
2024-05-15 21:20:06 +0300

    Retro

--- Assets/Scripts/SaveManager.cs
+++ Assets/Scripts/SaveManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
-using static SaveManager.Save;
 using System;
 
 public class SaveManager : MonoBehaviour
